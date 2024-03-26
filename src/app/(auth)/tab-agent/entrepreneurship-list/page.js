8d3140d7dd7afe188ce34d9703682a467f6b34ac
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-03-26 16:48:23 -0400

    View title page entrepreneurship

---
+++ src/app/(auth)/tab-agent/entrepreneurship-list/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Listentrepreneurship() {
+  return (
+    <section id="entrepreneurship-list">
+      <div className="info-page">
+        <p className="title-page">Todos los emprendimientos</p>
+        <span>
+          Estas viendo una lista con todos los emprendimientos de la plataforma.
+        </span>
+      </div>
+      <div className="content-page">
+        
+      </div>
+    </section>
+  );
+}
