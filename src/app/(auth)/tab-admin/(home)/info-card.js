e9648dbf0509e158959ee403624fbc85d742952c
Yisustxz <jesusoortiz2410@gmail.com>
2024-03-28 16:15:58 -0400

    feat: contents for the layout have been added

--- src/app/(auth)/tab-admin/(home)/info-card.js
+++ src/app/(auth)/tab-admin/(home)/info-card.js
@@ -5,7 +5,7 @@ const InfoCards = () => {
   return (
     <>
   <div className='row w-100 gap-3 '>
-                <div className='d-flex admin-content-layout col-sm-6'>
+                <div className='d-flex justify-content-between admin-content-layout col-sm-6'>
                   <div className='item-data'>
                     <span>Total de propiedades</span>
                     <h2>583</h2>  
@@ -15,56 +15,56 @@ const InfoCards = () => {
                    </div>
                 </div>
 
-                <div className='d-flex admin-content-layout col-sm-6'>
+                <div className='d-flex justify-content-between admin-content-layout col-sm-6'>
                   <div className='item-data'>
-                    <span>Total de propiedades</span>
+                    <span>Total de Emprendimientos</span>
                     <h2>583</h2>  
                   </div>
                   <div className="admin-content-icon">
-                      <Image src="/images/tab-agent/property.svg" alt="icon" height={40} width={40} className='img bg-custom' />
+                      <Image src="/images/tab-admin/lupa.svg" alt="icon" height={40} width={40} className='img bg-custom' />
                    </div>
                 </div>
               
 
-                <div className='d-flex admin-content-layout col-sm-6'>
+                <div className='d-flex justify-content-between admin-content-layout col-sm-6'>
                   <div className='item-data'>
-                    <span>Total de propiedades</span>
+                    <span>Total de barrios</span>
                     <h2>583</h2>  
                   </div>
-                  <div className="admin-content-icon">
-                      <Image src="/images/tab-agent/property.svg" alt="icon" height={40} width={40} className='img bg-custom' />
+                  <div className="admin-content-icon ">
+                      <Image src="/images/tab-admin/texto.svg" alt="icon" height={40} width={40} className='img bg-custom' />
                    </div>
                 </div>
               
 
-                <div className='d-flex admin-content-layout col-sm-6'>
+                <div className='d-flex justify-content-between admin-content-layout col-sm-6'>
                   <div className='item-data'>
-                    <span>Total de propiedades</span>
+                    <span>Total de Localidades</span>
                     <h2>583</h2>  
                   </div>
                   <div className="admin-content-icon">
-                      <Image src="/images/tab-agent/property.svg" alt="icon" height={40} width={40} className='img bg-custom' />
+                      <Image src="/images/tab-admin/corazon.svg" alt="icon" height={40} width={40} className='img bg-custom' />
                    </div>
                 </div>
               
 
-                <div className='d-flex admin-content-layout col-sm-6'>
+                <div className='d-flex justify-content-between admin-content-layout col-sm-6'>
                   <div className='item-data'>
-                    <span>Total de propiedades</span>
+                    <span>Total de Agentes</span>
                     <h2>583</h2>  
                   </div>
                   <div className="admin-content-icon">
-                      <Image src="/images/tab-agent/property.svg" alt="icon" height={40} width={40} className='img bg-custom' />
+                      <Image src="/images/tab-admin/texto.svg" alt="icon" height={40} width={40} className='img bg-custom' />
                    </div>
                 </div>
               
-                <div className='d-flex admin-content-layout col-sm-6'>
+                <div className='d-flex justify-content-between admin-content-layout col-sm-6'>
                   <div className='item-data'>
-                    <span>Total de propiedades</span>
+                    <span>Total de Clientes</span>
                     <h2>583</h2>  
                   </div>
                   <div className="admin-content-icon">
-                      <Image src="/images/tab-agent/property.svg" alt="icon" height={40} width={40} className='img bg-custom' />
+                      <Image src="/images/tab-admin/texto.svg" alt="icon" height={40} width={40} className='img bg-custom' />
                    </div>
                 </div>              
           </div>
--- src/app/(auth)/tab-admin/(home)/info-row.js
+++ src/app/(auth)/tab-admin/(home)/info-row.js
@@ -4,59 +4,75 @@ import Image from 'next/image'
 const InfoRows = () => {
   return (
     <>
-    <div className='row w-50 pt-3 '>
-                <div className='admin-content-info-layout'>
-                    <span className='admin-content-info-header'>Ultimas Actividades</span>
+    <div className='card-container row pt-3 '>
+            <div className='admin-content-info-layout'>
+                    <span className='admin-content-info-header'>Ultimas Actividades</span>   
+
+                    <div className='admin-content-info-row'>
+                      <div className='admin-content-info-icon'>
+                            <Image src="/images/tab-agent/property.svg" alt="icon" height={20} width={20} className='img bg-custom' />
+                          </div>
+                          <div className='admin-content-info'>
+                              <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span> a solicitado una visita a<span className='admin-content-info-span'> Nombre de la propiedad</span></p>
+                          </div>
+                    </div>
+
+                    <div className='admin-content-info-row'>
+                          <div className='admin-content-info-icon'>
+                            <Image src="/images/tab-admin/texto.svg" alt="icon" height={20} width={20} className='img bg-custom' />
+                          </div>
+                          <div className='admin-content-info'>
+                              <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span> modifico la información del emprendimiento </p>
+                          </div>
+                    </div>
+
+                    <div className='admin-content-info-row'>
+                          <div className='admin-content-info-icon'>
+                            <Image src="/images/tab-admin/corazon.svg" alt="icon" height={20} width={20} className='img bg-custom' />
+                          </div>
+                          <div className='admin-content-info'>
+                              <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span> Guardo en su lista la propiedad</p>
+                          </div>
+                    </div>
+
+                    <div className='admin-content-info-row'>
+                          <div className='admin-content-info-icon'>
+                            <Image src="/images/tab-agent/property.svg" alt="icon" height={20} width={20} className='img bg-custom' />
+                          </div>
+                          <div className='admin-content-info'>
+                              <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span> ha realizado una oferta de permuta en la propiedad</p>
+                          </div>
+                    </div>
+
+                    <div className='admin-content-info-row'>
+                          <div className='admin-content-info-icon'>
+                            <Image src="/images/tab-admin/texto.svg" alt="icon" height={20} width={20} className='img bg-custom' />
+                          </div>
+                          <div className='admin-content-info'>
+                              <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span>ha solicitado enviado una consulta al agente</p>
+                          </div>
+                    </div>
                     
-                <div className='admin-content-info-row'>
-                      <div className='admin-content-info-icon'>
-                        <Image src="/images/tab-agent/property.svg" alt="icon" height={20} width={20} className='img bg-custom' />
-                      </div>
-                      <div className='admin-content-info'>
-                          <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span> a solicitado una visita a <span  className='admin-content-info-span'>Nombre de la propiedad</span></p>
-                      </div>
-                </div>
-                <div className='admin-content-info-row'>
-                      <div className='admin-content-info-icon'>
-                        <Image src="/images/tab-agent/property.svg" alt="icon" height={20} width={20} className='img bg-custom' />
-                      </div>
-                      <div className='admin-content-info'>
-                          <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span> a solicitado una visita a <span  className='admin-content-info-span'>Nombre de la propiedad</span></p>
-                      </div>
-                </div>
-                <div className='admin-content-info-row'>
-                      <div className='admin-content-info-icon'>
-                        <Image src="/images/tab-agent/property.svg" alt="icon" height={20} width={20} className='img bg-custom' />
-                      </div>
-                      <div className='admin-content-info'>
-                          <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span> a solicitado una visita a <span  className='admin-content-info-span'>Nombre de la propiedad</span></p>
-                      </div>
-                </div>
-                <div className='admin-content-info-row'>
-                      <div className='admin-content-info-icon'>
-                        <Image src="/images/tab-agent/property.svg" alt="icon" height={20} width={20} className='img bg-custom' />
-                      </div>
-                      <div className='admin-content-info'>
-                          <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span> a solicitado una visita a <span  className='admin-content-info-span'>Nombre de la propiedad</span></p>
-                      </div>
-                </div>
-                <div className='admin-content-info-row'>
-                      <div className='admin-content-info-icon'>
-                        <Image src="/images/tab-agent/property.svg" alt="icon" height={20} width={20} className='img bg-custom' />
-                      </div>
-                      <div className='admin-content-info'>
-                          <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span> a solicitado una visita a <span  className='admin-content-info-span'>Nombre de la propiedad</span></p>
-                      </div>
-                </div>
-                <div className='admin-content-info-row'>
-                      <div className='admin-content-info-icon'>
-                        <Image src="/images/tab-agent/property.svg" alt="icon" height={20} width={20} className='img bg-custom' />
-                      </div>
-                      <div className='admin-content-info'>
-                          <p className='admin-content-info'><span className='admin-content-info-span'>Nombre del usuario</span> a solicitado una visita a <span  className='admin-content-info-span'>Nombre de la propiedad</span></p>
-                      </div>
-                </div>
-                </div>
+                    <div className='admin-content-info-row'>
+                          <div className='admin-content-info-icon'>
+                            <Image src="/images/tab-admin/corazon.svg" alt="icon" height={20} width={20} className='img bg-custom' />
+                          </div>
+                          <div className='admin-content-info'>
+                              <p className='admin-content-info'>Someone favorites your <span className='admin-content-info-span'>Triple Story House for Rent </span> listing </p>
+                          </div>
+                    </div>
+
+                    <div className='admin-content-info-row'>
+                          <div className='admin-content-info-icon'>
+                            <Image src="/images/tab-agent/property.svg" alt="icon" height={20} width={20} className='img bg-custom' />
+                          </div>
+                          <div className='admin-content-info'>
+                              <p className='admin-content-info'>Someone favorites your <span className='admin-content-info-span'>Triple Story House for Rent </span> listing </p>
+                          </div>
+                    </div>
+
+                    
+              </div>
 
           </div>
 
