5c4c33d2d6c97049a363fbe567772437e0dd1db5
M4Uu <crosslln1718@gmail.com>
2024-04-18 19:34:08 -0400

    feat(tab-client):addViewComparisons

---
+++ src/components/tab-client/comparisons/list.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+
+const TableComparisons = dynamic(() => import('./atoms/table'), { ssr: false })
+
+export default function FavoriteListClient() {
+  return (
+    <main id='list-favorites-view'>
+      <div className="row p30">
+        <TableComparisons/>
+      </div>
+      <div className="row p10">
+        {/* Begin Pagination */}
+        <div className="pagination-container">
+          {/* <Pagination pages={pages} lastPage={lastPage} range={range} actualPage={page}/> */}
+        </div>
+        {/* End Pagination */}
+      </div>
+    </main>
+  )
+}
