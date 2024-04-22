ceccaf73a009f81fc5048f51ceff5605f43b291c
Wilhelm Flores <wiljoseflores@gmail.com>
2024-04-22 17:49:28 -0400

    feat(FavoritesView): integration

---
+++ src/core/infrastructure/services/tab-client.service.ts
@@ -0,0 +1,8 @@
+import { ApiResponse } from "@/core/domain/response"
+import { ApiInstance } from "../api"
+import { Datum, PropertiesResponse } from "@/core/domain/responses/properties"
+
+export const getFavoritesClient = async function (page: string | number) {
+  const { data } = await ApiInstance<PropertiesResponse>(`/auth/get-favorites?page=${page}`)
+  return data
+}
---
+++ src/data/pagination.ts
@@ -0,0 +1,31 @@
+export const paginas = {
+  "current_page": 1,
+  "from": 1,
+  "last_page": 1,
+  "links": [
+      {
+          "url": null,
+          "label": "&laquo; Previous",
+          "active": false
+      },
+      {
+          "url": "http://jl.api.merakioffice.com/auth/get-favorites?page=1",
+          "label": "1",
+          "active": true
+      },
+      {
+        "url": "http://jl.api.merakioffice.com/auth/get-favorites?page=1",
+        "label": "2",
+        "active": false
+    },
+      {
+          "url": null,
+          "label": "Next &raquo;",
+          "active": false
+      }
+  ],
+  "path": "http://jl.api.merakioffice.com/auth/get-favorites",
+  "per_page": 15,
+  "to": 1,
+  "total": 1
+}
