468ebb79dbb3aa62743724789f53038767336331
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2024-04-25 12:41:11 -0400

    fix(comparisons-client): fix

--- src/components/tab-client/comparisons/atoms/comparisons/comparisons.js
+++ src/components/tab-client/comparisons/atoms/comparisons/comparisons.js
@@ -8,15 +8,15 @@ export default function Comparisons({comparisons}) {
     return(
         <>  
             <BodyComparisons key={comparisons?.real_state_1.id} element={comparisons?.real_state_1} typecomp={"Venta"} />
-            {
+            <BodyComparisons key={comparisons?.real_state_2?.id} element={comparisons?.real_state_2} typecomp={"Alquiler"} />
+            <BodyComparisons key={comparisons?.real_state_3?.id} element={comparisons?.real_state_3} typecomp={"Alquiler Temporal"}/>
+            {/* {
                 comparisons?.real_state_2 ? () => {
-                        <BodyComparisons key={comparisons?.real_state_2?.id} element={comparisons?.real_state_2} typecomp={"Alquiler"} />
                     comparisons?.real_state_3 ?
-                            <BodyComparisons key={comparisons?.real_state_3?.id} element={comparisons?.real_state_3} typecomp={"Alquiler Temporal"}/>
                     : ''
                 }
                 : ''
-            }
+            } */}
         </>
     )
 }
