fda698a14e9a5aae88014e5a707eaabe405ab45f
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2024-04-27 11:33:37 -0400

    feature(view-start): Styles of the review section completed. Visual phase finished

--- src/app/(pages)/start/page.js
+++ src/app/(pages)/start/page.js
@@ -165,43 +165,45 @@ const Start = () => {
                     <div className='container-reviews'>
                         <div className='header-reviews'>
                             <div className='container-text'>
-                                <span className='big-text'>Las personas hablan por nosotros</span>
+                                <h3 className='big-text'>Las personas hablan por nosotros</h3>
                                 <span className='little-text'>Estos son clientes más felices</span>
                             </div>
                             {/* Begin of Component "ControlReviews" */}
                             <div className='control-reviews'>
                                 <div className='control'>
-                                    <button className='control-arrow'>▶</button>
-                                    <div className='container-page'>
-                                        <div className='circle-page'>⚪</div>
-                                        <div className='circle-page'>⚫</div>
+                                    <button className='control-arrow active'><Image width={14} height={8.75} src={'/images/image-start/arrow-reviews-left.svg'} alt='arrow reviews left'/></button>
+                                    <div className='container-bullet'>
+                                        <div className='pagination-bullet'><Image className='active' width={6.4} height={6.4} src={'/images/image-start/pagination-bullet-reviews.svg'} alt='pagination bullet'/></div>
+                                        <div className='pagination-bullet'><Image className='' width={6.4} height={6.4} src={'/images/image-start/pagination-bullet-reviews.svg'} alt='pagination bullet'/></div>
                                     </div>
-                                    <button className='control-arrow'>◀</button>
+                                    <button className='control-arrow'><Image width={14} height={8.75} src={'/images/image-start/arrow-reviews-right.svg'} alt='arrow reviews right'/></button>
                                 </div>
                                 <span className='text-control'>VER EN GOOGLE</span>
                             </div>
                             {/* End of Component "ControlReviews" */}
                         </div>
+                        <div className='list-reviews'>
                         {/* Begin of Component "ElementReview" */}
-                        <div className='review'>
-                            <span className='title-review'>Great Work</span>
-                            <p className='text-review'>Amazing design, easy to customize and a design quality superlative account on its cloud platform for the optimized performance. And we didn’t on our original designs.</p>
-                            <ul className='stars-container'>
-                                <li className='star'>⭐</li>
-                                <li className='star'>⭐</li>
-                                <li className='star'>⭐</li>
-                                <li className='star'>⭐</li>
-                                <li className='star'>⭐</li>
-                            </ul>
-                            <div className='user-review'>
-                                <Image className='user-image' width={30} height={30} src={""} alt={"user image"}/>
-                                <div className='info-user'>
-                                    <span className='name-user'>Nombre de usuario</span>
-                                    <span className='user-job'>Trabajo de usuario</span>
+                            <div className='review'>
+                                <span className='title-review'>Great Work</span>
+                                <p className='text-review'>Amazing design, easy to customize and a design quality superlative account on its cloud platform for the optimized performance. And we didn’t on our original designs.</p>
+                                <ul className='stars-container'>
+                                    <li className='star'>⭐</li>
+                                    <li className='star'>⭐</li>
+                                    <li className='star'>⭐</li>
+                                    <li className='star'>⭐</li>
+                                    <li className='star'>⭐</li>
+                                </ul>
+                                <div className='user-review'>
+                                    <Image className='user-image' width={30} height={30} src={"/images/image-start/owner-review1.png"} alt={"user image"}/>
+                                    <div className='info-user'>
+                                        <span className='name-user'>Nombre de usuario<br/>Apellido de usuario</span>
+                                        <span className='user-job'>Trabajo de usuario</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         {/* End of Component "ElementReview" */}
+                        </div>
                     </div>
                     {/* End of Component "Reviews" */}
                 </section>
