17aa09f8982f80126c5acc15576e087309405fe1
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2024-04-30 08:19:45 -0400

    feature(public-appraisals): styling completed

--- src/app/tasaciones/page.js
+++ src/app/tasaciones/page.js
@@ -27,7 +27,7 @@ const About = () => {
       <MobileMenu />
       {/* End Mobile Nav  */}
 
-      <section className="breadcumb-section2 p-0"> </section>
+      <section className="breadcumb-section2 p-0" data-aos="fade-up"> </section>
 
 
       {/* Our About Area */}
@@ -78,71 +78,65 @@ const About = () => {
       </section>
       {/* End Our About Area */}
 
-      {/* About Banner */}
-      {/*       <section className="our-about pt-0">
-        <div className="container">
-          <div className="row" data-aos="fade-up" data-aos-delay="300">
-            <div className="col-lg-12">
-              <div className="about-page-img">
-                <Image
-                  width={1206}
-                  height={515}
-                  priority
-                  className="w-100 h-100 cover"
-                  src="/images/about/about-page-banner.jpg"
-                  alt="about banner"
-                />
-              </div>
-            </div>
+      {/* Begin of types of appraisals */}
+      <section className="types-apparaisals d-flex flex-column align-items-center">
+          <h1 data-aos="fade-down">Tipos de <strong>tasaciones</strong></h1>
+        <div className="container-type d-flex justify-content-between" data-aos="fade-down">
+          <div className="element-type">
+            <span className="title-type">Tasación esperada</span>
+            <p className="apparaisals-text">Es el precio de venta se espera que paguen por la propiedad, en donde el tiempo no tiene que ser un parámetro para la venta.</p>
+          </div>
+          <div className="element-type">
+            <span className="title-type">Tasación Posible</span>
+            <p className="apparaisals-text">Es el valor que posiciona tu propiedad junto a otras posibilidades del mercado de similares características en la misma zona. Acompaña el tiempo de venta.</p>
+          </div>
+          <div className="element-type">
+            <span className="title-type"> Tasación Dinamica</span>
+            <p className="apparaisals-text">Es la tasación que genera visitas físicas porque los potenciales compradores perciben una oportunidad en el mercado. Reduce el tiempo de venta.</p>
           </div>
         </div>
-      </section> */}
-      {/* End About Banner */}
+      </section>
+      {/* End of types of appraisals */}
 
-      <section>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-6 d-flex align-items-center justify-content-end">
-             <form className="contact-form">
-                <input type="text" name="name" placeholder="Nombre" />
-                <input type="text" name="email" placeholder="Email" />
-                <input type="text" name="phone" placeholder="Telefono" />
-                <input type="text" name="buld-land" placeholder="¿Tienes terreno donde construir?" />
-                <input type="text" name="land-size" placeholder="Metros cuadrados del terreno" />
-                <input type="text" name="start-date" placeholder="¿Cuando te gustaria comenzar la obra?" />
-                <input type="text" name="type" placeholder="Tipo de propiedad a construir" />
-                <input type="text" name="location" placeholder="Localidad donde vas a construir" />
-                <textarea placeholder="Cuentamos mas..." name="about"></textarea>
+
+
+      <section className="request">
+        <h1 data-aos="zoom-out">Titulo a resolver</h1>
+        <div className="container-request" data-aos="zoom-out">
+            <div className="container-form">
+             <form className="form-contact">
+                <input type="text" name="name" placeholder="Tipo de propiedad (Selección)" />
+                <input type="text" name="email" placeholder="Email (Selección)" />
+                <input type="text" name="phone" placeholder="Telefono (No obligatorio)" />
+                <input type="text" name="buld-land" placeholder="Partido (No obligatorio)" />
+                <input type="text" name="land-size" placeholder="Email" />
+                <input type="text" name="start-date" placeholder="Nombre completo" />
+                <input type="text" name="type" placeholder="Teléfono / Whatsapp" />
+                <textarea placeholder="Información extra que tengas de la propiedad" name="about"></textarea>
                 <input type="submit" value="ENVIAR SOLICITUD" />
               </form>
             </div>
             <div className="col-6 form-right-side"></div>
-          </div>
         </div>  
       </section>
-
-
-      {/* <section className="pt-0"> */}
-      {/*   <div className="row"> */}
-      {/*     <CustomPropertyGallery /> */}
-      {/*   </div> */}
-      {/* </section> */}
-
-      {/* Funfact */}
-      {/* <section className="pt-0"> */}
-      {/*   <div className="container"> */}
-      {/*     <div */}
-      {/*       className="row justify-content-center" */}
-      {/*       data-aos="fade-up" */}
-      {/*       data-aos-delay="300" */}
-      {/*     > */}
-      {/*       <FunFact /> */}
-      {/*     </div> */}
-      {/*   </div> */}
-      {/* </section> */}
-      {/* End Funfact */}
-
       {/* Exclusive Agents */}
+
+      {/* Begin of email send */}
+      <section className="email-send d-flex justify-content-center">
+        <div className="container-email d-flex">
+          <div className="info-send" data-aos="fade-right">
+            <h1>Enterate de todo</h1>
+            <p>Recibí más información de la zona, las ultimas novedades del rubro y las nuevas propiedades disponibles</p>
+          </div>
+          <div className="input-email" data-aos="fade-left">
+            <label>
+              <input placeholder="Email"></input>
+            </label>
+            <button>ENVIAR</button>
+          </div>
+        </div>
+      </section>
+      {/* End of email send */}
 
 
       {/* Our Partners */}
