a7d8cbb5f7f8c08624450607562e293a477beff8
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2024-05-01 14:40:17 -0400

    feature(layout-aboutus): create Page "AboutUs"

---
+++ src/app/(pages)/aboutus/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+import HeaderV2 from "@/components/common/HeaderV2";
+import Footer from "@/components/common/generic-footer";
+import { Contact, MapContact } from "@/components/pages/start/start-components/contactcomponents";
+import EmailSend from "@/components/tasaciones/emailsend";
