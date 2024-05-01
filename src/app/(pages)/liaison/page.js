8f41c0f2fe11a25a472156ae60c7ab92505a2676
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2024-05-01 13:00:46 -0400

    feature(layout-liaison): Layout of "Contact" page finished

---
+++ src/app/(pages)/liaison/page.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+import HeaderV2 from "@/components/common/HeaderV2";
+import Footer from "@/components/common/generic-footer";
+import { MapContact, Contact } from "@/components/pages/start/start-components/contactcomponents";
+import EmailSend from "@/components/tasaciones/emailsend";
+
+export const metadata = {
+    title: "Contact  || Homez - Real Estate NextJS Template",
+};
+
+export default function Liaison(){
+    return(
+        <main id="tab-liaison">
+            <HeaderV2/>
+            <div id="container-content">
+                <section id="section-questions">
+                    <div id="container-questions">
+                        <h1>Contacto</h1>
+                        <div className="container-form">
+                            <div className="container-form-left">
+                                <span>Tienes preguntas ? Ponte en contacto !</span>
+                                <form>
+                                    <input type="text" name="name" placeholder="Nombre"/>
+                                    <input type="text" name="phone" placeholder="Teléfono"/>
+                                    <input type="text" name="email" placeholder="Email"/>
+                                    <textarea placeholder="Escribe aquí tu consulta"></textarea>
+                                    <input type="button" name="send" value="ENVIAR"/>
+                                </form>
+                                <span>También puedes encontrarnos en nuestras redes</span>
+                                <div className="image-media">
+                                    <Image width={20} height={20} src={"/images/liaison/icon-form/facebook.svg"} alt="facebook icon"/>
+                                    <Image width={20} height={20} src={"/images/liaison/icon-form/instagram.svg"} alt="instagram icon"/>
+                                    <Image width={20} height={20} src={"/images/liaison/icon-form/youtube.svg"} alt="youtube icon"/>
+                                    <Image width={20} height={20} src={"/images/liaison/icon-form/soptify.svg"} alt="spotify icon"/>
+                                </div>
+                            </div>
+                            <Image className="image-side-right" width={1200} height={1200} src={"/images/liaison/image-side-form.png"} alt={"image side right"}/>
+                        </div>
+                    </div>
+                </section>
+                <section id="section-email">
+                    <EmailSend/>
+                </section>
+                <section id="section-schedule">
+                    <div className="container-schedule">
+                        <span>Visita nuestra oficina</span>
+                        <p>Puedes hacerlo durante la semana desde las 8AM a 12AM y desde las 15PM a 19PM</p>
+                    </div>
+                </section>
+                <section id="section-contact">
+                    <MapContact/>
+                    <Contact/>
+                </section>
+            </div>
+            <Footer/>
+        </main>
+    )
+}
