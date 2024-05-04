5fa56f173a3e797e893b1b44b40420681f637d9e
Wilhelm Flores <wiljoseflores@gmail.com>
2024-05-04 18:39:59 -0400

    Revert "fix(layout-listagent): minor fix"
    This reverts commit 19eec8a8c838afda0b3925a69ae81d7ff2fd9cda.

--- src/app/(auth)/tab-admin/list-agent/page.jsx
+++ src/app/(auth)/tab-admin/list-agent/page.jsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 const ListAgent = dynamic(() => import('@/components/tab-admin/list-agent/list-agent'), { ssr: false });
 
+import './list-agent.scss'
+
 const ListAgentPage = () => (
     <div id="tab-admin">
         <div id="content">
