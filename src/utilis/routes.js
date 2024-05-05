71db6bb2b8ea584b56ad7f662b3045984d90223f
José Vielma <josevglod@gmail.com>
2024-05-04 22:02:52 -0400

    add new routes to object

--- src/utilis/routes.js
+++ src/utilis/routes.js
@@ -19,8 +19,17 @@ export const ROUTES = {
   entrepreneurshipList: '/tab-agent/entrepreneurship-list',
   newProperty: '/tab-agent/new-property',
   propertyList: '/tab-agent/property-list',
+  agentProfile: '/tab-agent/profile',
+  //Client Routes
+  clientHome: "/tab-client",
+  clientFavorites: "/tab-client/favorites",
+  clientSearchs: "/tab-client/search",
+  clientComparisons: "/tab-client/comparisons",
+  clientRequests: "/tab-client",
   // Admin Routes 
   adminHome: '/tab-admin',
+  formWhoWeAre: '/tab-admin/form-whoweare',
+  formHome: '/tab-admin/form-home',
   newAgent: '/tab-admin/new-agent',
   tabClient: '/tab-client'
 }
