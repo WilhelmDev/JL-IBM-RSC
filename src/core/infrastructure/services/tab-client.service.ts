ce866fbba208c62475edfcbcb521a4d0354731bb
José Vielma <josevglod@gmail.com>
2024-05-07 20:47:12 -0400

    add getEntrepreneurshio and addFavorite services

--- src/core/infrastructure/services/tab-client.service.ts
+++ src/core/infrastructure/services/tab-client.service.ts
@@ -1,6 +1,7 @@
 import { ApiResponse } from "@/core/domain/response"
 import { ApiInstance } from "../api"
 import { Datum, PropertiesResponse } from "@/core/domain/responses/properties"
+import { Datum as Entrepreneurship } from "@/core/domain/responses/entreprenureships"
 import { Search } from "@/core/domain/search"
 
 export const getFavoritesClient = async function (page: string | number) {
@@ -16,4 +17,17 @@ return data
 export const getSearchClient = async function (page: string | number) {
   const { data } = await ApiInstance<Search>(`/auth/search?page=1`)
   return data
+}
+
+export const getEntrepreneurship = async function (id: number): Promise<Entrepreneurship> {
+  const { data } = await ApiInstance(`/entreprenureships/${id}`)
+  return data.data
+}
+
+export const addFavorite = async function (type: string, id: number) {
+  const { data } = await ApiInstance.post(`/auth/toggle-favorite`, {
+    type,
+    id
+  })
+  return data.data
 }
