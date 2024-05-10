69e3fdd9e5a2055ef7bfe4c2f2e74530cbd9d236
José Vielma <josevglod@gmail.com>
2024-05-10 13:59:33 -0400

    add neigborhood info integration

--- src/components/home/home-v3/propertie.js
+++ src/components/home/home-v3/propertie.js
@@ -1,49 +1,41 @@
 import Image from "next/image";
 
-const PropertieComponent = () => {
+const PropertieComponent = ({ neighborhood }) => {
+    /*${neighborhood.media.images.find(({front_page}) => front_page === 1).url}*/
     return (
         <>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12 wow fadeInUp" data-wow-delay="100">
                         <div className="text-start">
-                            <h4>Esta propiedad pertenece al barrio [Nombre del barrio]</h4>
+                            <h4>Esta propiedad pertenece al barrio [{neighborhood.title}]</h4>
                         </div>
-                        <div style={{ backgroundImage: 'url(/images/home-v3/district.png)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', width: '100%', height: '700px', margin: '0px auto' }}></div>
+                        <div style={{ backgroundImage: `url(/images/home-v3/district.png)`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', width: '100%', height: '700px', margin: '0px auto' }}></div>
                     </div>
                     <div className="col-lg-12 mt-5 wow fadeInUp" data-wow-delay="100">
                         <div className="text-start">
-                            <p className="mb-0">Descripción del emprendimiento.</p>
-                            <a href="#" className="fw-bold">Leer mas</a>
+                            <p className="mb-0">{neighborhood.description}</p>
+                            {/*<a href="#" className="fw-bold">Leer mas</a>*/}
                         </div>
                     </div>
 
                     <div className="col-lg-12 mt-3 mb-4 wow fadeInUp" data-wow-delay="100">
                         <div className="container">
-                            <p className="fw-bold mb-2">Amenidades de [Nombre del barrio]</p>
+                            <p className="fw-bold mb-2">Amenidades de [{neighborhood.title}]</p>
                             <div className="row ms-1">
-                                <div className="col-lg-4">
-                                    <p className="mb-0">Calle</p>
-                                    <p className="mb-0">Seguridad 24 Hrs</p>
-                                    <p className="mb-0">Pileta</p>
-                                    <p className="mb-0">Multiespacio</p>
-                                </div>
-                                <div className="col-lg-4">
-                                    <p className="mb-0">Restaurant</p>
-                                    <p className="mb-0">Sala de cine</p>
-                                    <p className="mb-0">Alambre perimetral</p>
-                                    <p className="mb-0">Mantenimiento de jardines</p>
-                                </div>
-                                <div className="col-lg-4">
-                                    <p className="mb-0">Limpieza de calles</p>
-                                    <p className="mb-0">Recolección de basura</p>
-                                    <p className="mb-0">Atención Medica</p>
-                                    <p className="mb-0">Wi Fi publico</p>
-                                </div>
+                                {
+                                    neighborhood.details.amenidades.map((amenidad, index) => {
+                                        return (
+                                            <div key={index} className="col-lg-4">
+                                                <p className="mb-0">{amenidad.name}</p>
+                                            </div>
+                                        )
+                                    })
+                                }
                             </div>
                         </div>
                     </div>
-                    <a href="#" className="fw-bold mb-5">Ver Propiedades del barrio</a>
+                    {/*<a href="#" className="fw-bold mb-5">Ver Propiedades del barrio</a>*/}
                 </div>
             </div>
         </>
