f23c727a3d37ef8f6a39cab6c77a52e50cdd7d00
José Vielma <josevglod@gmail.com>
2024-05-30 10:36:41 -0400

    remove useless imports

--- src/components/tab-admin/form-services/tabs/Maintenance.js
+++ src/components/tab-admin/form-services/tabs/Maintenance.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import UploadMediaServices from '../uploadMedia'
 import { updateMaintenanceService } from '@/core/infrastructure/services/tab-admin.service'
 import { toast } from 'react-toastify'
--- src/components/tab-admin/form-services/tabs/hero.js
+++ src/components/tab-admin/form-services/tabs/hero.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import UploadVideoCustom from '../uploadVideo'
 import { FrontOptions } from "@/data/selects";
 import Select from "react-select";
